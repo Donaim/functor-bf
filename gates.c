77c73e3ca4b5830d35eff430c88e6093e361d508
Donaim <0@kek.com>
2018-04-14 14:44:33 +0200

    optimized C version

--- gates.c
+++ gates.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
 #include "indexed_succ.h"
 
 #define uint unsigned int
 #define uchar unsigned char
+
 #define CORELEN 2
 #define MAX_OP_NUM 7
 
 typedef struct {
     uchar a, b, c, d;
 } func;
-typedef struct {
-    func arr[CORELEN + MAX_OP_NUM];
-    uint size;
-} farr;
 typedef struct {
     Pair arr[MAX_OP_NUM];
     uint size;
@@ -30,6 +25,12 @@ func tnand(const func x, const func y) {
     re.d = nand(x.d, y.d);
     return re;
 }
+#define tnandf(x, y, re) \
+    re.a = nand(x.a, y.a); \
+    re.b = nand(x.b, y.b); \
+    re.c = nand(x.c, y.c); \
+    re.d = nand(x.d, y.d); \
+
 #define func_equal(x, y) (x.a == y.a && x.b == y.b && x.c == y.c && x.d == y.d)
 
 void print_functor(func o) {
@@ -41,86 +42,39 @@ const func Y = { 0, 0, 1, 1 };
 const func T = { 1, 1, 1, 1 };
 const func F = { 0, 0, 0, 0 };
 
-
-uint powi(uint base, uint x) {
-    uint re = 1;
-    while (base-- > 0) { 
-        re *= x;
-    }
-    return re;
-}
-uint get_count(uint num_steps, uint min_num) {
-    uint re = 1;
-    for (int i = min_num, to = min_num + num_steps; i < to; ++i) {
-        re *= (powi(2, i));
-    }
-
-    // printf("count(%d,%d) = %d\n", num_steps, min_num, re);
-
-    return re;
-}
-
-Pair ** create_sc(uint poss, uint oplen) {
-    Pair ** re = (Pair**)malloc(poss * sizeof(Pair*));
-    for (uint i = 0; i < poss; ++i) {
-        re[i] = (Pair*)malloc(oplen * sizeof(Pair));
-    }
-    return re;
-}
-void free_sc(Pair ** sc, uint poss) {
-    for (uint i = 0; i < poss; i++) {
-        free(sc[i]);
-    }
-    free(sc);
-}
-
 oparr bruteforce(const func desired) {
     printf("\nsearching for: ");
     print_functor(desired);
 
-    func core[4];
-    core[0] = X;
-    core[1] = Y;
-    core[2] = T;
-    core[3] = F;
+    func core[4] = { X, Y, T, F };
 
+    uchar bases[MAX_OP_NUM * 2];
+    uchar counter[MAX_OP_NUM * 2];
+    for (int i = 0; i < MAX_OP_NUM * 2; i++) 
+    { 
+        counter[i] = 0;
+        bases[i]   = ((CORELEN + (i + 2)) / 2); 
+    }
+
+    func be[CORELEN + MAX_OP_NUM];
+    for (int j = 0; j < CORELEN; j++) { be[j] = core[j]; }
+    
     for (int oplen = 0; oplen <= MAX_OP_NUM; oplen++) {
-        uint poss_count = get_count(oplen, CORELEN);
-        Pair ** sc = create_sc(poss_count, oplen);
-
-        for (int o = 0; o < oplen; o++) {
-            Pair pair = { 0, 0 };
-            uint base = CORELEN + o;
-            for (int k = 0; k < poss_count; k++) {
-                // printf("pair = ");
-                // print_pair(pair);
-                // printf("\n");
-                sc[k][o] = pair;
-                increment_pair(&pair, base);
+        do {
+            for (int k = 0, j = 0; k < oplen; ++k, j += 2) {
+                Pair p = { counter[j], counter[j + 1] };
+                tnandf(be[p.a], be[p.b], be[CORELEN + k]);
             }
-        }
-
-        farr be;
-        be.size = CORELEN + oplen;
-        for (int j = 0; j < CORELEN; j++) { be.arr[j] = core[j]; }
-
-        for (int i = 0; i < poss_count; ++i) {
-            for (int k = CORELEN; k < be.size; ++k) {
-                Pair p = sc[i][k - CORELEN];
-                be.arr[k] = tnand(be.arr[p.a], be.arr[p.b]);
-            }
-
-            if (func_equal(be.arr[be.size - 1], desired)) {
+            if (func_equal(be[CORELEN + oplen - 1], desired)) {
                 oparr re;
                 re.size = oplen;
-                for (int k = 0; k < oplen; k++) {
-                    re.arr[k] = sc[i][k];
+                for (int k = 0, j = 0; k < oplen; ++k, j += 2) {
+                    Pair p = { counter[j], counter[j + 1] };
+                    re.arr[k] = p;
                 }
                 return re;
             }
-        }
-
-        free_sc(sc, poss_count);
+        } while(increment_mult_bases(counter, oplen * 2, bases));
     }
 
     printf("\nCould not find ");
@@ -129,27 +83,25 @@ oparr bruteforce(const func desired) {
 }
 
 void find_hardest() {
-    Array inc;
-    inc.size = 4;
-    inc.ptr[0] = inc.ptr[1] = inc.ptr[2] = inc.ptr[3] = 0;
+    uchar inc[4] = { 0, 0, 0, 0 };
     
     func hardest_functor;
     uint hardest_oplen = 0;
     
     func nxor = { 1, 0, 0, 1 };
 
-    for (int i = 0, to = powi(2, 4); i < to; i++) {
-        func desired = { inc.ptr[0], inc.ptr[1], inc.ptr[2], inc.ptr[3] };
-        increment_arr(&inc, 2);
+    do {
+        func desired = { inc[3], inc[2], inc[1], inc[0] };
+        // if (func_equal(desired, nxor)) { continue; }
 
         oparr found = bruteforce(desired);
-        printf("found in %d operations\n", found.size);
+        // printf("found in %d operations\n", found.size);
 
         if (found.size > hardest_oplen) {
             hardest_oplen = found.size;
             hardest_functor = desired;
         }
-    }
+    } while (increment_arr(inc, 4, 2));
 
     printf("\nchecked all. hardest was: ");
     print_functor(hardest_functor);
@@ -163,12 +115,12 @@ void find_xor() {
     for (int i = 0; i < found.size; i++) {
         print_pair(found.arr[i]);
         if (i != found.size - 1) { printf("->"); }
-        else { printf("\n"); }
     }
+    printf("\n");
 }
 
 int main() {
-    // find_xor();
+    find_xor();
     find_hardest();
 
     return 0;
--- indexed_succ.h
+++ indexed_succ.h
@@ -2,66 +2,49 @@
 
 #include <stdio.h>
 
-#define ARRSIZE 4
 #define uint unsigned int
 #define uchar unsigned char
 
 typedef struct {
-    uchar ptr[ARRSIZE];
-    uchar size;
-} Array;
-typedef struct {
     uchar a, b;
-} Pair;
+} Pair; 
 
 void print_pair(Pair p) {
     printf("(%d,%d)", p.a, p.b);
 }
 
-void print_arr_rev(Array a) {
-    printf("%c ", '{');
-    for (int i = a.size - 1; i >= 0; --i) {
-        printf("%d ", a.ptr[i]);
+int increment_arr(uchar * arr, uint size, uint base) {
+     for (int i = 0; i < size; i++) {
+        if (arr[i] + 1 < base) {
+            ++arr[i];
+            return 1;
+        }
+        arr[i] = 0;
     }
-    printf("%c\n", '}');
+    return 0;
+}
+int increment_mult_bases(uchar * arr, uint size, uchar * bases) { // return 0 on overflow
+    for (int i = 0; i < size; i++) {
+        if (arr[i] + 1 < bases[i]) {
+            ++arr[i];
+            return 1;
+        }
+        arr[i] = 0;
+    }
+    return 0;
 }
 
-int find_last_index(uchar * arr, uint size, uint base)
-{
-    for (int i = 0; i < size; ++i) {
-        if (arr[i] < base) { return i; }
-    }
-    return -1;
-}
-void increment_arr(Array * A, uint base) {
-    --base;
-    uchar * arr = A->ptr;
-    if (arr[0] < base) {
-        ++arr[0];
-    } else {
-        int last_index = find_last_index(arr, A->size, base);
-        if (last_index > 0) {
-            ++arr[last_index];
-        } else {
-            last_index = A->size;
-            arr[last_index] = 1;
-            A->size++;
-        }
-
-        for (int i = 0; i < last_index; ++i) {
-            arr[i] = 0;
-        }
-    }
-}
-void increment_pair(Pair * p, uint base) {
-    if (p->a < base - 1) {
+int increment_pair(Pair * p, uint base) {
+    if (p->a + 1 < base) {
         ++(p->a);
     } else 
-    if (p -> b < base - 1) {
+    if (p->b + 1 < base) {
         ++(p->b);
         p->a = 0;
     } else { // overflow
         p->a = 0;
         p->b = 0;
+        return 0;
     }
+    return 1;
 }
